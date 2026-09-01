273297e8d2944ea0801567fc897f5344428805db
crabtsai <87925842+crabtsai@users.noreply.github.com>
2026-09-01 22:16:23 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Web 前端組態檔
 // 此為 Firebase Web App 公開組態；請勿在此放入 Service Account、私鑰或伺服器密鑰。
 window.FIREBASE_CONFIG = Object.freeze({
-  apiKey: "AIzaSyASYC9LQ2WtFFUj0jbiaosR6A3pqBUUnoc",
-  authDomain: "aiapplicantionsingle.firebaseapp.com",
-  projectId: "aiapplicantionsingle",
-  storageBucket: "aiapplicantionsingle.firebasestorage.app",
-  messagingSenderId: "742013125249",
-  appId: "1:742013125249:web:4ff7eadd8c2856c6750380",
-  measurementId: "G-MLF5FYS6F4"
+  apiKey: "AIzaSyChsmSHNElQYVUNSiblN6LiYKbScP5EpkY",
+  authDomain: "ips-ai-application-basic-exam.firebaseapp.com",
+  projectId: "ips-ai-application-basic-exam",
+  storageBucket: "ips-ai-application-basic-exam.firebasestorage.app",
+  messagingSenderId: "1096961993447",
+  appId: "1:1096961993447:web:9d4633a21476f192f31042",
+  measurementId: "G-JW9645CDJP"
 });
